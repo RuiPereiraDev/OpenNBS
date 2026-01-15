839d0f18d5c867e12f6d3ddc49ddaab005f795b3
Rui Pereira <me@ruipereira.dev>
2026-01-15 21:26:27 +0000

    Add tests for tempo change functionality

--- src/test/kotlin/dev/ruipereira/opennbs/SongTest.kt
+++ src/test/kotlin/dev/ruipereira/opennbs/SongTest.kt
@@ -3,6 +3,7 @@ package dev.ruipereira.opennbs
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 class SongTest {
@@ -29,5 +30,31 @@ class SongTest {
         assertEquals(0, song.loopStartTick)
         assertTrue(song.layers.isEmpty())
         assertTrue(song.customInstruments.isEmpty())
+    }
+
+    @Test
+    @OptIn(TempoChangeAPI::class)
+    fun `test song tempo changes`() {
+        val resource = this::class.java.classLoader.getResourceAsStream("tempo.nbs")
+        val song = OpenNBS.decode(resource!!)
+
+        assertTrue(song.hasTempoChanger)
+        assertEquals(1000, song.getTempoAtTick(-1))
+        assertEquals(2000, song.getTempoAtTick(0))
+        assertEquals(4000, song.getTempoAtTick(20))
+        assertEquals(666, song.getTempoAtTick(80))
+        assertEquals(666, song.getTempoAtTick(100))
+    }
+
+    @Test
+    @OptIn(TempoChangeAPI::class)
+    fun `test song no tempo change inst`() {
+        val resource = this::class.java.classLoader.getResourceAsStream("tempo.nbs")
+
+        assertNotNull(resource)
+        val song = OpenNBS.decode(resource).copy(customInstruments = emptyList())
+
+        assertFalse(song.hasTempoChanger)
+        assertEquals(1000, song.getTempoAtTick(0))
     }
 }
